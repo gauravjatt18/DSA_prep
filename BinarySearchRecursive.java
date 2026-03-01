c6299ebf94217d2b7b4711418cfa86ce002b30fa
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-01 20:14:23 +0530

    Initial commit

---
+++ BinarySearchRecursive.java
@@ -0,0 +1,23 @@
+public class BinarySearchRecursive {
+
+    public static int binarySearch(int[] arr, int left, int right, int target) {
+        if (left > right) return -1;
+
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] > target)
+            return binarySearch(arr, left, mid - 1, target);
+        else
+            return binarySearch(arr, mid + 1, right, target);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int target = 7;
+
+        int result = binarySearch(arr, 0, arr.length - 1, target);
+        System.out.println("Index: " + result);
+    }
+}
