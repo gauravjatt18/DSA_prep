340c85651f7b793dcf3341bd251f9a5403f14ebc
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-02 21:29:41 +0530

    Initial commit

---
+++ SecondLargest.java
@@ -0,0 +1,34 @@
+public import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements: ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + secondLargest);
+        sc.close();
+    }
+} {
+    
+}
