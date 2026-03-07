95fb0871c842a9a935416ff24ae2f6eb7ad0ad02
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-08 00:04:02 +0530

    Initial commit

---
+++ EvenOddArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class EvenOddArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Even numbers:");
+        for(int i = 0; i < n; i++) {
+            if(arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        System.out.println("\nOdd numbers:");
+        for(int i = 0; i < n; i++) {
+            if(arr[i] % 2 != 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
