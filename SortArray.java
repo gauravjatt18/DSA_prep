3dc5bfbeefc0900d2097f5e5aeaea02857e4f6c1
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-07 23:39:38 +0530

    Initial commit

---
+++ SortArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SortArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for(int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = i + 1; j < 5; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
