5616d54ec27acef37a2baeacd6392e2c9d6b7d07
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-14 22:15:21 +0530

    Initial commit

---
+++ day3.java
@@ -0,0 +1,39 @@
+
+
+
+public class day3 {
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        int temp;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Array before sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("\nArray after sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
