4a6cdeee023e22366ec2d7e90e8ab503754b3fa7
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-20 00:09:02 +0530

    Initial commit

---
+++ w2.java
@@ -0,0 +1,54 @@
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+public class CopyRandomList {
+
+    public static Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        Node curr = head;
+
+        // STEP 1: Insert copied nodes
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        // STEP 2: Set random pointers
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        // STEP 3: Separate lists
+        curr = head;
+        Node dummy = new Node(0);
+        Node copyCurr = dummy;
+
+        while (curr != null) {
+            copyCurr.next = curr.next;
+            curr.next = curr.next.next;
+
+            curr = curr.next;
+            copyCurr = copyCurr.next;
+        }
+
+        return dummy.next;
+    }
+} {
+    
+}
