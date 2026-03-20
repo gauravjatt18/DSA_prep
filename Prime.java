c0cce501ac65ce6f617336acf6b9dee0f3beb78d
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-21 00:19:40 +0530

    Initial commit

---
+++ Prime.java
@@ -0,0 +1,20 @@
+public class Prime {
+    public static void main(String[] args) {
+        int num = 7;
+        boolean isPrime = true;
+
+        for(int i = 2; i <= num/2; i++) {
+            if(num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime && num > 1)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not Prime");
+    }
+} {
+    
+}
