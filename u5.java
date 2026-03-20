ce7987bf7f0de32704495ec39313a3a889994862
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-21 00:24:19 +0530

    Initial commit

---
+++ u5.java
@@ -0,0 +1,11 @@
+class ExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch(ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Done");
+        }
+    }
+}
