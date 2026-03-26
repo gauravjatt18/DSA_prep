87e8a32d28255ee1ce6c579d64e47e059d0a6abe
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-27 00:42:58 +0530

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// File Name: HelloWorld.java
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("My name is Gaurav Chaudhary");
+        System.out.println("Batch - 12, SAP ID - 500127055");
+
+    }
+}
+ 
