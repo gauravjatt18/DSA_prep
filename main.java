3018683f6e3a9225346ee301d71ac1ae3637d4a0
gauravjatt18 <gauravjaat456780@gmail.com>
2026-03-30 14:11:19 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,38 @@
+
+
+ public class Main {
+    public static void main(String[] args){
+        int age = 36;
+        int year =3535;
+        int quantity= 1;
+        
+        double price =  33.4;
+        double gap = 6.6;
+        double tem =-23;
+        
+        char grade ='D';
+        char symbol ='$';
+        char currency ='@';
+        
+        boolean isStudent = false;
+        boolean forSale =false;
+        boolean isonline = true;
+        String  name  ="gaurav chaudahry";
+        
+        String food ="pizzz";
+        String email = "fake454@gamil.com";
+        
+        System.out.println(" your email is"  + email);
+        
+        
+        if(isStudent){
+            System.out.println("you are a student!");
+            
+        }
+        else {
+              System.out.println("you are  not a student!");
+        
+        }
+    }    
+  
+}
