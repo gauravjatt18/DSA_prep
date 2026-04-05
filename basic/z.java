370352a0a2900d4af94d50af760228cc3df0b3b5
gauravjatt18 <gauravjaat456780@gmail.com>
2026-04-05 16:19:44 +0530

    Initial commit

---
+++ basic/z.java
@@ -0,0 +1,9 @@
+public class z {
+    public static void main(String[] args) {
+        // area of circle
+        double r = 4.2;
+        double area = Math.PI*r*r;
+        System.out.println("Area of circle is:" + area);
+
+    }
+}
