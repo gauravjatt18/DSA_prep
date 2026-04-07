57283bd66d88432c44a48a44e489a43b81d384fb
gauravjatt18 <gauravjaat456780@gmail.com>
2026-04-07 18:27:41 +0530

    Initial commit

---
+++ basic/loops/basicloops.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class basicloops {
+    
+}
+import java.util.Scanner;
+
+public class AP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i = 2; i <= 3*n - 1; i += 3){
+            System.out.print(i + " ");
+        }
+        
+        sc.close();
+    }
+}
